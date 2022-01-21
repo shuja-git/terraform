709bea017c0e8a67d63436896853e29e2dbe416f
shuja <shuja.devops@gmail.com>
2022-01-21 11:59:25 +0530

    variable list

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -6,27 +6,50 @@
 #}
 #variable "empty" {}
 
-variable "string" {
-  description = "String data type demo"
-  type = string
-  default = "This is String Data type variable"
+#variable "string" {
+#  description = "String data type demo"
+#  type = string
+#  default = "This is String Data type variable"
+#}
+#variable "number" {
+#  description = "Number data type demo"
+#  type = number
+#  default = 100
+#}
+#variable "boolean" {
+#  description = "boolean data type demo"
+#  type =  bool
+#  default = true
+#}
+#output "printing" {
+#  value = var.string
+#  }
+#output "number" {
+#  value = var.number
+#}
+#output "boolean" {
+#  value = var.boolean
+#}
+#--------------------------Variables of list of values
+variable "demo1" {
+  description = "List of variables"
+  default = [
+    "Hello",
+    1000,
+    true
+  ]
 }
-variable "number" {
-  description = "Number data type demo"
-  type = number
-  default = 100
-}
-variable "boolean" {
-  description = "boolean data type demo"
-  type =  bool
-  default = true
-}
-output "printing" {
-  value = var.string
+variable "demo2" {
+  description = "Map values"
+  default = {
+    Course = "DevOps",
+    timing = "6am",
+
   }
-output "number" {
-  value = var.number
 }
-output "boolean" {
-  value = var.boolean
+output "sample1" {
+  value = var.demo1[0]
+  }
+output "sample2" {
+  value = var.demo2["Course"]
 }
