65c468f0435ca4e9a427f14e1c938fd3dc6c4e9f
shuja <shuja.devops@gmail.com>
2022-01-23 17:09:52 +0530

    t3micro

--- 03-resources/resources.tf
+++ 03-resources/resources.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 resource "aws_instance" "sample" {
   ami = "ami-04656078adf4aa403"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 }
 output "out" {
   value = aws_instance.sample
