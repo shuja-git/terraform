6d02c5434cdb3eec91c59346c655a34582e23b4c
shuja <shuja.devops@gmail.com>
2022-01-24 15:29:35 +0530

    t3.micro

--- 04-modules/ec2/main.tf
+++ 04-modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample" {
   ami           = "ami-04656078adf4aa403"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [var.sg_id]
 }
 variable "sg_id" {}
