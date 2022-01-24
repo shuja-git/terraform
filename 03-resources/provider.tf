2f7319b858b47ec659a23a4b3ae7f163b8c760c2
shuja <shuja.devops@gmail.com>
2022-01-24 15:31:45 +0530

    first tfstate file commented

--- 03-resources/provider.tf
+++ 03-resources/provider.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 
 }
-
-terraform {
-  backend "s3" {
-    bucket = "terraform-shbucket"
-    key    = "sample.terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-shbucket"
+#    key    = "sample.terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
