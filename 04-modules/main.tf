d71386db27dcea7c7e00b7139255ca461240dfe5
shuja <shuja.devops@gmail.com>
2022-01-24 15:24:00 +0530

    tfstate stored remotely in s3

--- 04-modules/main.tf
+++ 04-modules/main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+terraform {
+  backend "s3" {
+    bucket = "terraform-shbucket"
+    key    = "04-modules/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 module "sg" {
   source = "./sg"
 }
